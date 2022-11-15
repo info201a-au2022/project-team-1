9612e9e57638851d54213de1018a082b8192d281
Kim Nguyen <kimng@uw.edu>
2022-11-15 00:43:33 -0800

    fixed relative paths for the charts. Set working directory to main branch

--- source/pie_tt_spotify_22.R
+++ source/pie_tt_spotify_22.R
@@ -1,11 +1,11 @@
 library(dplyr)
 library(ggplot2)
 
-spotify_22 <- read.csv("../data/spotify_charts_2022.csv") %>%
+spotify_22 <- read.csv("data/spotify_charts_2022.csv") %>%
   select(artist_names, track_name, peak_rank, weeks_on_chart) %>%
   rename(artist = artist_names, song_title = track_name)
 
-tiktok_22 <- read.csv("../data/TikTok_songs_2022.csv") %>%
+tiktok_22 <- read.csv("data/TikTok_songs_2022.csv") %>%
   select(track_name, artist_name, track_pop) %>%
   rename(song_title = track_name, artist = artist_name)
 
@@ -14,15 +14,12 @@ chart_22 <- left_join(spotify_22, tiktok_22, by = c("song_title", "artist")) %>%
 
 # attempting to make a pie chart
 
-test_chart_22 <- chart_22 
+test_chart_22 <- chart_22
 
 test_chart_22[is.na(test_chart_22)] = "0" 
 
 test_chart_22 <- test_chart_22 %>%
   mutate(on_both = track_pop > 0)
-
-test_chart_22 <- chart_22 %>%
-  mutate(on_both = (track_pop >= 0)) 
 
 summary <- test_chart_22 %>%
   group_by(on_both) %>%
