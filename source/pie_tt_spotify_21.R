2edf9da1e70ab3c07dc85768702c13f11b2e1d33
Kim Nguyen <kimng@uw.edu>
2022-11-28 01:03:44 -0800

    made the pie chart for 2021

---
+++ source/pie_tt_spotify_21.R
@@ -0,0 +1,43 @@
+library(dplyr)
+library(ggplot2)
+
+# Charts for 2020 Spotify and TikTok
+# Please change the wd() to /docs later, but for now we are going to be 
+# working with /source
+# working from /docs would be "data/CSV FILE.csv"
+spotify_21 <- read.csv("../data/spotify_charts_2021.csv") %>%
+  select(artist_names, track_name, peak_rank, weeks_on_chart) %>%
+  rename(artist = artist_names, song_title = track_name)
+
+tiktok_21 <- read.csv("../data/TikTok_songs_2021.csv") %>%
+  select(track_name, artist_name, track_pop) %>%
+  rename(song_title = track_name, artist = artist_name)
+
+# Join both charts together by song title and artist in order to make a pie chart
+chart_21 <- left_join(spotify_21, tiktok_21, by = c("song_title", "artist")) %>%
+  arrange(-peak_rank) 
+
+# Converting all NAs in the `track_pop` column into 0s
+chart_21[is.na(chart_21)] = "0"
+
+# Adding another column `on_both` to see whether a song was popular on both charts
+chart_21 <- chart_21 %>%
+  mutate(on_both = track_pop > 0)
+
+# New summary variable summarizes how many FALSEs and TRUEs there are
+summary_21 <- chart_21 %>%
+  group_by(on_both) %>%
+  summarize(length(on_both)) %>%
+  rename(value = "length(on_both)")
+
+# Make a stacked bar chart to have all the data
+pie_21 <- ggplot(summary_21, aes(x = "Song",
+                                 y = value,
+                                 fill = on_both)) +
+  geom_bar(stat = "identity")
+
+# Transform into a pie chart
+pie_21 <- pie_21 +
+  coord_polar("y", start = 0) +
+  geom_text(aes(label = paste0(round((value/nrow(chart_21) * 100)), "%")),
+            position = position_stack(vjust = 0.5))
