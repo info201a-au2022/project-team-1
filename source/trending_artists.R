dc7264d286d939b125120a5d5f523d5bebc51ffe
mdunbar11 <dunbar11@uw.edu>
2022-11-28 21:22:52 -0800

    Made master data frame

--- source/trending_artists.R
+++ source/trending_artists.R
@@ -90,7 +90,17 @@ joined_songs_19 <- joined_songs_19 %>%
 View(joined_songs_19)
 
 # Building master dataframe for app purposes
-master_dataframe <- full_join(joined_songs_19, joined_songs_20, joined_songs_21, joined_songs_22, by = c("year")) %>%
+master_dataframe <- full_join(joined_songs_19, joined_songs_20, by = c("year")) %>%
+  arrange(year)
+
+View(master_dataframe)
+
+master_dataframe <- full_join(master_dataframe, joined_songs_21, by = c("year")) %>%
+  arrange(year)
+
+View(master_dataframe)
+
+master_dataframe <- full_join(master_dataframe, joined_songs_22, by = c("year")) %>%
   arrange(year)
 
 View(master_dataframe)
