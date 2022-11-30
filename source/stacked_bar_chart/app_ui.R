965223042509a215ba50c2700f9f8e2e7405fb6d
mdunbar11 <dunbar11@uw.edu>
2022-11-30 10:41:07 -0800

    saved changes

--- source/stacked_bar_chart/app_ui.R
+++ source/stacked_bar_chart/app_ui.R
@@ -1,2 +1,15 @@
 library(shiny)
 library(plotly)
+
+barchart_side_content <- sidebarPanel(
+  selectInput(
+    "artistvar",
+    label = "Select artist",
+    choices = unique(data$artist)
+  )
+)
+
+barchart_main_content <- mainPanel(
+  plotlyOutput("barchart")
+)
+  
