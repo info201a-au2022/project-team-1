7c17be389120e454b1fd5907a63feea704bd83f0
mdunbar11 <dunbar11@uw.edu>
2022-11-29 21:44:59 -0800

    built app files

--- source/stacked_bar_chart/build_bar_chart.R
+++ source/stacked_bar_chart/build_bar_chart.R
@@ -3,6 +3,11 @@ library(stringr)
 
 source("./trending_artists.R")
 
-build_bar_chart <- function(data, artistvar, xvar = "year", yvar = nrow("year")) {
-  
+build_bar_chart <- function(data, artistvar, xvar = "year", yvar = sum(data$year == "year")) {
+  p <- plot_ly(x= data[, xvar],
+               y = data[, yvar],
+               color = ~artistvar,
+               type = bar,
+               ) %>%
+    layout(barmode = "stack")
 }
